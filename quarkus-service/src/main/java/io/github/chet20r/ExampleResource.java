3fbc31f57a3481b26b8f971212580db7e3fe46d5
Chetan Raju <chet20r@gmail.com>
2021-02-16 16:04:25 +1100

    bug: http 301 issue with quarkus

--- quarkus-service/src/main/java/io/github/chet20r/ExampleResource.java
+++ quarkus-service/src/main/java/io/github/chet20r/ExampleResource.java
@@ -11,7 +11,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/calculator")
+@Path("/")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class ExampleResource {
--- quarkus-service/src/main/java/io/github/chet20r/cxf/interceptor/BasicAuthInterceptor.java
+++ quarkus-service/src/main/java/io/github/chet20r/cxf/interceptor/BasicAuthInterceptor.java
@@ -1,21 +1,35 @@
 package io.github.chet20r.cxf.interceptor;
 
+import io.quarkus.arc.Unremovable;
+import org.apache.cxf.binding.soap.SoapMessage;
 import org.apache.cxf.interceptor.Fault;
 import org.apache.cxf.message.Message;
 import org.apache.cxf.phase.AbstractPhaseInterceptor;
 import org.apache.cxf.phase.Phase;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
-import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Singleton;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
-@ApplicationScoped
-public class BasicAuthInterceptor extends AbstractPhaseInterceptor<Message> {
+@Unremovable
+@Singleton
+public class BasicAuthInterceptor extends AbstractPhaseInterceptor<SoapMessage> {
+
+    private String someProperty;
+
     public BasicAuthInterceptor() {
         super(Phase.PRE_PROTOCOL);
-        System.out.println("**************Initializing the basic auth interceptor**************");
+    }
+
+    void setup(@ConfigProperty(name = "some.property", defaultValue = "abc") String someProperty) {
+        this.someProperty = someProperty;
     }
 
     @Override
-    public void handleMessage(Message message) throws Fault {
-        System.out.println("=====================Initializing the basic auth interceptor========================");
+    public void handleMessage(SoapMessage soapMessage) throws Fault {
+        var headers = (Map<String, List<?>>) soapMessage.get(Message.PROTOCOL_HEADERS);
+        headers.put("User-Agent", Collections.singletonList(someProperty));
     }
 }
